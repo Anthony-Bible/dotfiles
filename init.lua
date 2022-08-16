3ae271e52fcd65e637c2590d270a98a98f916a81
Anthony Bible <anthony.bible@tcnbroadcasting.com>
2022-08-16 15:08:54 -0600

    change to work on mac and automatically set up packer

---
+++ init.lua
@@ -0,0 +1,37 @@
+-- [[init.lua ]]
+
+-- LEADER
+-- These keybindings need to be defined before the first /
+-- is called; otherwise, it will default to "\"
+vim.g.mapleader = ","
+vim.g.localleader = "\\"
+
+-- IMPORTS
+
+require('vars')      -- Variables
+
+require('keys')      -- Keymaps
+
+require('plug')      -- Plugin
+require('lsp_config')
+require('opts')      -- Options
+-- PLUGINS: Add this section
+require('nvim-tree').setup{}
+
+-- Add the block below
+require('lualine').setup {
+  options = {
+    theme = 'dracula-nvim'
+  }
+}
+require('nvim-autopairs').setup{} 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    autocmd BufWritePre *.go lua vim.lsp.buf.formatting()
+    autocmd BufWritePre *.go lua goimports(1000)
+  augroup end
+]])
+
+--call deoplete#custom#option('omni_patterns', { 'go': '[^. *\t]\.\w*' })
---
+++ lua/plug.lua
@@ -0,0 +1,41 @@
+-- [[ plug.lua ]]
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
+return require('packer').startup(function(use)
+  use {                                              -- filesystem navigation
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons'        -- filesystem icons
+  }
+   use { 'mhinz/vim-startify' }                       -- start screen
+  use { 'DanilaMihailov/beacon.nvim' }               -- cursor jump
+  use {
+    'nvim-lualine/lualine.nvim',                     -- statusline
+    requires = {'kyazdani42/nvim-web-devicons',
+                opt = true}
+  }
+  use { 'Mofiqul/dracula.nvim' }
+  use 'shaunsingh/nord.nvim'
+
+
+  -- [[ Dev ]]
+  use {
+    'nvim-telescope/telescope.nvim',                 -- fuzzy finder
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use { 'majutsushi/tagbar' }                        -- code structure
+  use { 'Yggdroot/indentLine' }                      -- see indentation
+  use { 'tpope/vim-fugitive' }                       -- git integration
+  use { 'junegunn/gv.vim' }                          -- commit history
+  use { 'windwp/nvim-autopairs' }                    -- auto close brackets, etc.
+     use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+
+       if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
