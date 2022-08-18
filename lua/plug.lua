8c9b97cde2deaf9307092849784e5d2ba836b7ba
anthony bible <anthony@anthonybible.com>
2022-08-18 08:06:34 -0600

    feat: add surround.vim

--- lua/plug.lua
+++ lua/plug.lua
@@ -33,6 +33,7 @@ return require('packer').startup(function(use)
   use { 'tpope/vim-fugitive' }                       -- git integration
   use { 'junegunn/gv.vim' }                          -- commit history
   use { 'windwp/nvim-autopairs' }                    -- auto close brackets, etc.
+  use{ 'tpope/vim-surround' }
      use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
 
        if packer_bootstrap then
