53d9f8591372dc4a8cc2c309d6810f13d33e161f
Anthony Bible <anthony.bible@tcnbroadcasting.com>
2023-04-04 11:29:57 -0600

    don't change whitespace and source kubectl completions

--- nvim/lua/ale.lua
+++ nvim/lua/ale.lua
@@ -9,5 +9,5 @@ vim.g.ale_echo_msg_warning_str = ''
 vim.g.ale_echo_msg_format = '[%linter%] %s [%severity%]'
 vim.g.ale_linters_explicit = 1
 vim.g.ale_linters = {['javascript'] = {'eslint'}, ['go'] = {'revive'} }
-vim.g.ale_fixers = { ['*'] = { 'remove_trailing_lines', 'trim_whitespace' }, ['javascript'] = {'eslint'}  }
+vim.g.ale_fixers = { ['javascript'] = {'eslint'}  }
 vim.g.ale_fix_on_save = 1
