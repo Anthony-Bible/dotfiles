392cdfd6e9115c1ba5703cda29acac3017d6f5bf
anthony bible <anthony@anthonybible.com>
2023-04-07 07:38:33 -0600

    Setup wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+
+return {
+        font = wezterm.font 'Space Mono for Powerline',
+        font_size = 10.5,
+color_scheme = 'Dracula',
+window_background_opacity =0.95, 
+
+}
+
+
+
