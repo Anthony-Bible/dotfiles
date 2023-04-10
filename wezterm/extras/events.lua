654e189ffdae52def8e191b7385972ce49950ae4
anthony bible <anthony@anthonybible.com>
2023-04-10 07:37:20 -0600

    add key to save output to file

---
+++ wezterm/extras/events.lua
@@ -0,0 +1,55 @@
+local wezterm = require 'wezterm'
+local io = require 'io'
+local os = require 'os'
+local act = wezterm.action
+
+wezterm.on('trigger-vim-with-visible-text', function(window, pane)
+  -- Retrieve the current viewport's text.
+  --
+  -- Note: You could also pass an optional number of lines (eg: 2000) to
+  -- retrieve that number of lines starting from the bottom of the viewport.
+  local viewport_text = pane:get_lines_as_text()
+
+  -- Create a temporary file to pass to vim
+--  local name = os.tmpname()
+--  local f = io.open(name, 'w+')
+--  f:write(viewport_text)
+--  f:flush()
+--  f:close()
+
+  -- Get $HOME/.config/wezterm/logs
+  local custom_log_dir = wezterm.config_dir .. "/logs"
+  -- open a file in $HOME/.config/wezterm/logs with a date and timestamp
+  local log_name = custom_log_dir .. "/wezterm-" .. os.date("%Y-%m-%d-%H-%M-%S") .. ".log"
+  custom_log_file = io.open(log_name, "w+")
+  custom_log_file:write(viewport_text)
+  custom_log_file:flush()
+  custom_log_file:close()
+
+  -- Open a new window running vim and tell it to open the file
+  window:perform_action(
+    act.SpawnCommandInNewWindow {
+      args = { 'nvim', log_name},
+    },
+    pane
+  )
+
+  -- Wait "enough" time for vim to read the file before we remove it.
+  -- The window creation and process spawn are asynchronous wrt. running
+  -- this script and are not awaitable, so we just pick a number.
+  --
+  -- Note: We don't strictly need to remove this file, but it is nice
+  -- to avoid cluttering up the temporary directory.
+  wezterm.sleep_ms(5000)
+end)
+
+return {
+  keys = {
+    {
+      key = 'E',
+      mods = 'CTRL',
+      action = act.EmitEvent 'trigger-vim-with-visible-text',
+    },
+  },
+}
+
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,11 @@
 local wezterm = require 'wezterm'
-
+local events_functions = require 'extras.events'
 return {
         font = wezterm.font 'Space Mono for Powerline',
         font_size = 10.5,
 color_scheme = 'Dracula',
 window_background_opacity =0.95, 
-
+keys = events_functions.keys,
 }
 
 
