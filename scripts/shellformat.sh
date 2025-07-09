7d668ed6ecd8c9ac2e7167c764010d7b17a1888a
Anthony Bible <anthony@anthony.bible>
2025-07-09 08:07:24 -0600

    refactor(scripts): enhance shellformat.sh with batch processing and improve error handling

--- scripts/shellformat.sh
+++ scripts/shellformat.sh
@@ -1,27 +1,33 @@
 #!/usr/bin/env bash
-
+echoerr() {
+    echo -e "\033[31m$*\033[0m" >&2
+}
+#if [[ $# -lt 1 ]]; then
+#    echoerr "Usage: $0 <file(s)>"
+#    exit 1
+#fi
 if ! command -v shellcheck &> /dev/null; then
-    echo "shellformat is not installed. Please install it first."
-    exit 1
-fi
-
-if [[ -z "$1" ]]; then
-    echo "Usage: $0 <file>"
-    exit 1
-fi
-file="$1"
-
-if [[ ! -f "$file" ]]; then
-    echo "File not found: $file"
-    exit 1
-fi
-
-shellcheck_output=$(shellcheck "$file" --format=tty --severity=warning 2>&1)
-exit_code=$?
-
-if [[ $exit_code -ne 0 ]]; then
-    echo "Script has shellcheck errors, fix these one by one: $shellcheck_output" >&2
+    echoerr "shellcheck is not installed. Please install it first."
     exit 2
-else
-    echo "No shellcheck problems found"
 fi
+final_exit_code=0
+while IFS= read -r filepath; do
+    echo "Checking file: $filepath"
+    if [[ ! -f "$filepath" ]]; then
+        echoerr "File not found: $filepath"
+        exit 2
+    fi
+    shellcheck_output=$(shellcheck "$filepath" --format=tty --severity=warning 2>&1)
+    exit_code=$?
+
+    if [[ $exit_code -ne 0 ]]; then
+        echoerr "Script has shellcheck errors, fix these one by one: $shellcheck_output" 
+        final_exit_code=2
+    else
+        echo "No shellcheck problems found"
+    fi
+done 
+if  [[ $final_exit_code -eq 0 ]]; then
+    echo "All files passed shellcheck."
+fi
+exit $final_exit_code
